6ba88de2113d2ee726920a7c7af553b8979384c3
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-05-23 20:38:14 +0100

    start coding parsing file

--- parsing.py
+++ parsing.py
@@ -8,4 +8,24 @@ def parsing_file(file_path: str) -> Graph:
     nb_drones: int = 0
     zones: List[Zone] = []
     connections: List[Connection] = []
+    lines: List[str] = []
+
+    with open(file_path, 'r') as f:
+        lines = f.readlines()
+    for l in lines:
+        l = l.strip()
+        if l.startswith("#") or len(l) == 0:
+            continue
+        elif l.startswith("start_hub"):
+            ...
+        elif l.startswith("end_hub"):
+            ...
+        elif l.startswith("hub"):
+            ...
+        elif l.startswith("connection"):
+            ...
+        else:
+            ...
+        
+
     return Graph(nb_drones, zones, connections)
