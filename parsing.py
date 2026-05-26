0600b5d30b6cfed9552530b0341110f1ff765961
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-05-26 16:45:53 +0100

    finish parsing

--- parsing.py
+++ parsing.py
@@ -4,12 +4,10 @@ from zone import Zone
 from typing import List, Dict
 
 
-def get_nb_drones(line: str, nb_drones_found: bool) -> int:
+def get_nb_drones(line: str) -> int:
+
     nb_drones: int = 0
 
-    if nb_drones_found:
-        print("[ERROR]: nb_drones should not be duplicated")
-        exit()
     if ": " in line:
         s = line.split(": ")
     else:
@@ -21,7 +19,7 @@ def get_nb_drones(line: str, nb_drones_found: bool) -> int:
         return nb_drones
 
 
-def parse_metadata_zones(meta_data, line_num) -> Dict:
+def parse_metadata_zones(meta_data: str, line_num: int) -> Dict:
     default_data = {'color': None, 'zone_type': "normal",
                     'max_drones': 1
                     }
@@ -47,6 +45,10 @@ def parse_metadata_zones(meta_data, line_num) -> Dict:
             if data == "color":
                 default_data["color"] = value
             elif data == "zone":
+                valid = ["normal", "blocked", "restricted", "priority"]
+                if value not in valid:
+                    print(f"[ERROR]: invalid zone type '{value}'")
+                    exit()
                 default_data["zone_type"] = value
             elif data == "max_drones":
                 try:
@@ -89,6 +91,66 @@ def parse_hub(line: str, zones: List[Zone], hub_category: str,
     zones.append(zone)
 
 
+def parse_connection(line: str, connections: List[Connection],
+                     zones: List[Zone], line_num: int) -> None:
+
+    data = line.split()
+    if data and len(data) > 1 and data[1]:
+
+        names = data[1].split('-')
+        if len(names) == 2:
+            name1, name2 = names[0].strip(), names[1].strip()
+            if not name1 or not name2:
+                print("[ERROR]: One or both zones are empty")
+                exit()
+            name1_found = False
+            name2_found = False
+            for z in zones:
+                if name1 == z.name:
+                    name1_found = True
+                if name2 == z.name:
+                    name2_found = True
+            if name1_found is False or name2_found is False:
+                print("[ERROR]: One or both zones are empty")
+                exit()
+        else:
+            print("[ERROR]: Invalid format. Expected connection "
+                  "like this zone1-zone2.")
+            exit()
+        if len(data) > 2:
+            if "=" in data[2]:
+                data[2] = data[2].strip("[]")
+                _, value = data[2].split("=")
+                if not value:
+                    print("[ERROR]: metadata should be like "
+                          "this max_link_capacity=<value>")
+                    exit()
+                try:
+                    v = int(value)
+                except Exception:
+                    print("[ERROR]: coordinates must be integers")
+                    exit()
+                connection = Connection(name1, name2, v)
+            else:
+                print("[ERROR]: metadata should be like "
+                      "this max_link_capacity=<value>")
+                exit()
+        else:
+            connection = Connection(name1, name2, 1)
+        for c in connections:
+            if (
+                (connection.name1 == c.name1) and
+                    (connection.name2 == c.name2) or
+                    (connection.name1 == c.name2 and
+                     connection.name2 == c.name1)):
+                print("[ERROR]: connections should be unique")
+                exit()
+        connections.append(connection)
+    else:
+        print("[ERROR]: Data is missing or empty")
+        exit()
+
+
 def parsing_file(file_path: str) -> Graph:
     try:
         nb_drones: int = -1
@@ -106,7 +168,10 @@ def parsing_file(file_path: str) -> Graph:
             if line.startswith("#") or len(line) == 0:
                 continue
             if line.startswith("nb_drones"):
-                nb_drones = get_nb_drones(line, nb_drones_found)
+                if nb_drones_found:
+                    print("[ERROR]: nb_drones should not be duplicated")
+                    exit()
+                nb_drones = get_nb_drones(line)
                 if nb_drones == -1:
                     print("[ERROR]: nb_drones must be positive")
                     exit()
@@ -123,9 +188,16 @@ def parsing_file(file_path: str) -> Graph:
             elif line.startswith("hub"):
                 parse_hub(line, zones, "hub", False, i)
             elif line.startswith("connection"):
-                ...
+                parse_connection(line, connections, zones, i)
             else:
                 print(f"[ERROR] line <{i}>: invalid syntax")
+                exit()
+        if not start_hub_found:
+            print("[ERROR]: start_hub is missing")
+            exit()
+        if not end_hub_found:
+            print("[ERROR]: end_hub is missing")
+            exit()
         return Graph(nb_drones, zones, connections)
     except Exception as e:
         print("[ERROR]:", e)
