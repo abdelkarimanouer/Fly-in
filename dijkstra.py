c2fd2bb2419c1f68f8b85ee6400f5f99dd854699
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-06-07 14:51:10 +0100

    code djikstra algo

--- dijkstra.py
+++ dijkstra.py
@@ -1,20 +1,47 @@
 from graph import Graph
 from zone import Zone
-from typing import List
+from typing import List, Dict
 import heapq as h
 import math
 
 
 def dijkstra(graph: Graph, start: Zone, end: Zone) -> List[Zone]:
-    path = []
-    heap = []
+    path: List = []
+    heap: List = []
     visited = set()
-    parents = {}
-    costs = {}
+    parents: Dict = {}
+    costs: Dict = {}
 
     for z in graph.zones:
         costs[z.name] = math.inf
     costs[start.name] = 0
     h.heappush(heap, (0, start.name))
 
-    return path
+    while heap:
+        current_cost, name_zone = h.heappop(heap)
+        if name_zone in visited:
+            continue
+        visited.add(name_zone)
+
+        if name_zone == end.name:
+            z = graph.get_zone_from_zones_list(name_zone)
+            path.append(z)
+            while name_zone != start.name:
+                z = graph.get_zone_from_zones_list(parents[name_zone])
+                path.append(z)
+                name_zone = parents[name_zone]
+            path.reverse()
+            return path
+
+        for n in graph.get_neighbor_zone(name_zone):
+            if n.zone_type == "blocked":
+                continue
+            if n.name not in visited:
+                new_cost = current_cost + graph.get_zone_cost(n)
+                if new_cost < costs[n.name]:
+                    costs[n.name] = new_cost
+                    parents[n.name] = name_zone
+                    h.heappush(heap, (new_cost, n.name))
+
+    print("[ERROR]: No path found from start to end")
+    exit()
