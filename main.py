d2275b91077ce9ff27b8fa5a74e2247a3ba858e7
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-06-08 21:01:01 +0100

    start code simulation

--- main.py
+++ main.py
@@ -1,8 +1,22 @@
 from parsing import parsing_file
+from sys import argv
+from graph import Graph
+from drone import Drone
+from zone import Zone
+from simulation import simulation, assign_paths
+from typing import List
 
 
 def main() -> None:
-    parsing_file("maps/easy/01_linear_path.txt")
+    graph: Graph = parsing_file(argv[1])
+    for z in graph.zones:
+        if z.hub_category == "start_hub":
+            start: Zone = z
+        if z.hub_category == "end_hub":
+            end: Zone = z
+    drones: List[Drone] = assign_paths(graph, start, end, graph.nb_drones)
+    start.current_drones = graph.nb_drones
+    simulation(graph, drones, end)
 
 
 if __name__ == "__main__":
