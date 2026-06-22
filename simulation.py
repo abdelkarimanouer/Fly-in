c8141f24cf7ea71c004e9789ec03c32f9cf8953f
abdelkarimanouer <abdelkarimanouer1337@gmail.com>
2026-06-22 16:12:05 +0100

    start coding simulation

---
+++ simulation.py
@@ -0,0 +1,13 @@
+from zone import Zone
+from graph import Graph
+from typing import List
+from rich import print
+from webcolors import name_to_hex
+
+
+class Simulation:
+    @staticmethod
+    def assign_path(graph: Graph) -> List[List[Zone]]:
+        paths: List[List[Zone]] = []
+
+        return paths
